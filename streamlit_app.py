6fe588933b2acbd1cbc823df86546feed93257d1
andres <amanotas13@gmail.com>
2022-09-07 15:20:31 -0500

    remove raw text

--- streamlit_app.py
+++ streamlit_app.py
@@ -30,8 +30,6 @@ fruityvice_response = requests.get(
 
 streamlit.header("Fruityvice Fruit Advice!")
 
-streamlit.text(fruityvice_response.json())
-
 fruityvice_normalized = pd.json_normalize(fruityvice_response.json())
 
 streamlit.dataframe(fruityvice_normalized)
