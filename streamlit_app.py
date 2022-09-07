2eab04ba3abb5acc622fd6f0b740077655a66bd0
andres <amanotas13@gmail.com>
2022-09-07 18:37:16 -0500

    try catch kiwi

--- streamlit_app.py
+++ streamlit_app.py
@@ -30,18 +30,26 @@ streamlit.dataframe(fruits_to_show)
 
 streamlit.header("Fruityvice Fruit Advice!")
 
-fruit_choice = streamlit.text_input(
-    "What fruit would you like information about?", "Kiwi"
-)
+try:
+    fruit_choice = streamlit.text_input(
+        "What fruit would you like information about?", "Kiwi"
+    )
+    if not fruit_choice:
+        streamlit.error("Please select a fruit to get information.")
+    else:
 
-fruityvice_response = requests.get(
-    f"https://fruityvice.com/api/fruit/{fruit_choice}"
-)
+        fruityvice_response = requests.get(
+            f"https://fruityvice.com/api/fruit/{fruit_choice}"
+        )
 
-streamlit.write("The user entered", fruit_choice)
-fruityvice_normalized = pd.json_normalize(fruityvice_response.json())
+        streamlit.write("The user entered", fruit_choice)
+        fruityvice_normalized = pd.json_normalize(fruityvice_response.json())
 
-streamlit.dataframe(fruityvice_normalized)
+        streamlit.dataframe(fruityvice_normalized)
+
+except URLError as e:
+    streamlit.error(e)
+
 
 streamlit.stop()
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
