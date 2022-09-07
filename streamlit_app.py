3b242ef52f00b59069f5566e41476c022dbc3ab0
Andrés Manotas Cantillo <65831635+Amanotas20@users.noreply.github.com>
2022-09-07 10:26:49 -0500

    Update streamlit_app.py
    Add body

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,1 +1,6 @@
+import streamlit
 
+streamlit.title('My Parents New Healthy Dinner')
+streamlit.text('Omega 3 & Blueberry Oatmeal')
+streamlit.text('Kale, Spinach & Rocket Smootie')
+streamlit.text('Hard-Boiled Free-Range Egg')
