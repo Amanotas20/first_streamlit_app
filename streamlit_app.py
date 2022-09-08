e01e039df7377bf9f2d3975c0247a05bc819461e
andres <amanotas13@gmail.com>
2022-09-07 19:10:02 -0500

    fixed all buttons

--- streamlit_app.py
+++ streamlit_app.py
@@ -65,12 +65,24 @@ if streamlit.button("Get Fruit Load List"):
     my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
     my_cur = my_cnx.cursor()
     my_data_rows = get_fruit_load_list()
+    my_cnx.close()
     streamlit.dataframe(my_data_rows)
 
-streamlit.stop()
+
+def insert_row_snowflake(new_fruit):
+    with my_cnx.cursor() as my_cur:
+        my_cur.execute(
+            f"insert into fruit_load_list values ('{add_my_fruit}')"
+        )
+        return f"thanks for adding {add_my_fruit}"
+
+
 add_my_fruit = streamlit.text_input(
     "What fruit would you like to add?", "jackfruit"
 )
-streamlit.text(f"thanks for adding {add_my_fruit}")
 
-my_cur.execute("insert into fruit_load_list values ('')")
+if streamlit.button("Add a Fruit to the list"):
+    my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+    insert_row = insert_row_snowflake(add_my_fruit)
+    my_cnx.close()
+    streamlit.text(insert_row)
